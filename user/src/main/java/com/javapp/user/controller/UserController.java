29156d5df8ad73351516153bbe939779b81ce11c
llsrrll96 <llsrrll96@gmail.com>
2022-12-08 23:03:18 +0900

    feat: add annotation

--- user/src/main/java/com/javapp/user/controller/UserController.java
+++ user/src/main/java/com/javapp/user/controller/UserController.java
@@ -1,7 +1,15 @@
 package com.javapp.user.controller;
 
+import com.javapp.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
+    private final UserService userService;
+
+
 }
