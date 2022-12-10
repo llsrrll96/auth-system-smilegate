4590cf798d393f10455cc2bf5f7b01ce4daaf484
llsrrll96 <llsrrll96@gmail.com>
2022-12-10 22:49:07 +0900

    chore: Add findUserDetail, findUsers

--- user/src/main/java/com/javapp/user/controller/UserController.java
+++ user/src/main/java/com/javapp/user/controller/UserController.java
@@ -2,11 +2,14 @@ package com.javapp.user.controller;
 
 import com.javapp.user.dto.user.SignUpDto;
 import com.javapp.user.dto.user.UserDto;
+import com.javapp.user.entity.user.User;
 import com.javapp.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,9 +18,19 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    private ResponseEntity<UserDto> signUp(@RequestBody SignUpDto signUpDto){
+    public ResponseEntity<UserDto> signUp(@RequestBody SignUpDto signUpDto){
 
         UserDto userDto = userService.createUser(signUpDto);
         return new ResponseEntity<>(userDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> findUserDetail(@PathVariable Long userId){
+        return new ResponseEntity<>(userService.findUserDetail(userId), HttpStatus.OK);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserDto>> findUsers(){
+        return new ResponseEntity<>(userService.findByUsers(),HttpStatus.OK);
     }
 }
