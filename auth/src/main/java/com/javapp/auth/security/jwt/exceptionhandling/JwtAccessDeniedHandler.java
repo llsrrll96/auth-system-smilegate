261280f9e963674ed897cb51ee668d0ca1593e05
llsrrll96 <llsrrll96@gmail.com>
2022-12-10 22:46:00 +0900

    feat: create JwtAccessDeniedHandler

---
+++ auth/src/main/java/com/javapp/auth/security/jwt/exceptionhandling/JwtAccessDeniedHandler.java
@@ -0,0 +1,17 @@
+package com.javapp.auth.security.jwt.exceptionhandling;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// 권한없는 사용자 접근
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.sendRedirect("/denied");
+    }
+}
