23cdad5aa2498f80195c90dd1bd65660cf199b42
llsrrll96 <llsrrll96@gmail.com>
2022-12-10 11:37:41 +0900

    feat: add mapToUserEntity

--- user/src/main/java/com/javapp/user/service/UserServiceImpl.java
+++ user/src/main/java/com/javapp/user/service/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.javapp.user.service;
 
-import com.javapp.user.dto.UserDto;
-import com.javapp.user.entity.User;
+import com.javapp.user.dto.user.SignUpDto;
+import com.javapp.user.dto.user.UserDto;
+import com.javapp.user.entity.user.Role;
+import com.javapp.user.entity.user.User;
 import com.javapp.user.repository.UserJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,9 +16,23 @@ public class UserServiceImpl implements UserService{
     private final UserJpaRepository userJpaRepository;
 
     @Override
-    public UserDto createUser(User user) {
-        userJpaRepository.save(user);
-        return null;
+    public UserDto createUser(SignUpDto signUpDto) {
+        // 검증
+
+        User user = mapToUserEntity(signUpDto);
+
+        // 추후 인증서버를 통해 암호화된 비밀번호를 받아와 회원가입 진행
+
+        return new UserDto(userJpaRepository.save(user));
+    }
+
+    private User mapToUserEntity(SignUpDto signUpDto) {
+        return User.builder()
+                .username(signUpDto.getUsername())
+                .email(signUpDto.getEmail())
+                .password(signUpDto.getPassword())
+                .role(Role.ROLE_USER)
+                .build();
     }
 
     @Override
