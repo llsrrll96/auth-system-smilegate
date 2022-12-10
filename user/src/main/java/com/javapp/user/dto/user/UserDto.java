461850a79f2b1a8bf362e8b1532d7e05122bc59e
llsrrll96 <llsrrll96@gmail.com>
2022-12-10 22:49:53 +0900

    feat: Add createDateTime, modifiedDateTime

--- user/src/main/java/com/javapp/user/dto/user/UserDto.java
+++ user/src/main/java/com/javapp/user/dto/user/UserDto.java
@@ -1,9 +1,14 @@
 package com.javapp.user.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.javapp.user.entity.user.Role;
 import com.javapp.user.entity.user.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
@@ -14,12 +19,20 @@ public class UserDto {
 
     private String email;
 
-    private Role role;
+    private String role;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    private LocalDateTime createDateTime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    private LocalDateTime modifiedDateTime;
 
     public UserDto(User user) {
         this.userId = user.getUserId();
         this.username = user.getUsername();
         this.email = user.getEmail();
         this.role = user.getRole();
+        this.createDateTime = user.getCreateDateTime();
+        this.modifiedDateTime = user.getModifiedDateTime();
     }
 }
