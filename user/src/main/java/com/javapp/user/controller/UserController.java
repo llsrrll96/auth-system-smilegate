d6a7e7fb936f5b98a6360c83366901346ae42589
llsrrll96 <llsrrll96@gmail.com>
2022-12-10 11:39:07 +0900

    feat: add createUser

--- user/src/main/java/com/javapp/user/controller/UserController.java
+++ user/src/main/java/com/javapp/user/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.javapp.user.controller;
 
+import com.javapp.user.dto.user.SignUpDto;
+import com.javapp.user.dto.user.UserDto;
 import com.javapp.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,5 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
+    @PostMapping("/signup")
+    private ResponseEntity<UserDto> signUp(@RequestBody SignUpDto signUpDto){
 
+        UserDto userDto = userService.createUser(signUpDto);
+        return new ResponseEntity<>(userDto, HttpStatus.CREATED);
+    }
 }
