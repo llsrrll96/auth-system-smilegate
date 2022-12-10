9cc68a6e6d43b2c91c309658af9021ed9ed8da85
llsrrll96 <llsrrll96@gmail.com>
2022-12-10 11:38:34 +0900

    -

--- user/src/main/java/com/javapp/user/repository/UserJpaRepository.java
+++ user/src/main/java/com/javapp/user/repository/UserJpaRepository.java
@@ -1,6 +1,6 @@
 package com.javapp.user.repository;
 
-import com.javapp.user.entity.User;
+import com.javapp.user.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserJpaRepository extends JpaRepository<User,Long> {
