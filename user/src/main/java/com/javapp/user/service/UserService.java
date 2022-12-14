3feecdcd1a1b1b9453209d7f211c0c1c6cdf4b6f
llsrrll96 <llsrrll96@gmail.com>
2022-12-14 11:16:37 +0900

    chore: Delete createUser

--- user/src/main/java/com/javapp/user/service/UserService.java
+++ user/src/main/java/com/javapp/user/service/UserService.java
@@ -1,15 +1,11 @@
 package com.javapp.user.service;
 
-import com.javapp.user.dto.user.SignUpDto;
 import com.javapp.user.dto.user.UserDto;
 import com.javapp.user.entity.user.User;
 
 import java.util.List;
 
 public interface UserService {
-    // c
-    UserDto createUser(SignUpDto signUpDto);
-
     // r
     List<UserDto> findByUsers();
 
--- user/src/main/java/com/javapp/user/service/UserServiceImpl.java
+++ user/src/main/java/com/javapp/user/service/UserServiceImpl.java
@@ -1,8 +1,6 @@
 package com.javapp.user.service;
 
-import com.javapp.user.dto.user.SignUpDto;
 import com.javapp.user.dto.user.UserDto;
-import com.javapp.user.entity.user.Role;
 import com.javapp.user.entity.user.User;
 import com.javapp.user.repository.UserJpaRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,25 +14,6 @@ import java.util.stream.Collectors;
 public class UserServiceImpl implements UserService{
     private final UserJpaRepository userJpaRepository;
 
-    @Override
-    public UserDto createUser(SignUpDto signUpDto) {
-        // 검증
-
-        User user = mapToUserEntity(signUpDto);
-
-        // 추후 인증서버를 통해 암호화된 비밀번호를 받아와 회원가입 진행
-
-        return new UserDto(userJpaRepository.save(user));
-    }
-
-    private User mapToUserEntity(SignUpDto signUpDto) {
-        return User.builder()
-                .username(signUpDto.getUsername())
-                .email(signUpDto.getEmail())
-                .password(signUpDto.getPassword())
-                .role(Role.ROLE_USER.getType())
-                .build();
-    }
 
     private UserDto mapToUserDto(User user){
         return new UserDto(user);
