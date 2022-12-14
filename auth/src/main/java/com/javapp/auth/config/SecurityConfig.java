2218e6a3c74f61ba5b2b6adc1acf9574a225857c
llsrrll96 <llsrrll96@gmail.com>
2022-12-14 11:19:54 +0900

    feat: Add Beans
    JwtAuthenticationFiliter, AuthenticationManager, BCryptPasswordEncoder

--- auth/src/main/java/com/javapp/auth/config/SecurityConfig.java
+++ auth/src/main/java/com/javapp/auth/config/SecurityConfig.java
@@ -1,22 +1,36 @@
 package com.javapp.auth.config;
 
 
+import com.javapp.auth.security.jwt.JwtAuthenticationFiliter;
 import com.javapp.auth.security.jwt.exceptionhandling.JwtAccessDeniedHandler;
 import com.javapp.auth.security.jwt.exceptionhandling.JwtAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
+    public JwtAuthenticationFiliter jwtAuthenticationFiliter(){
+        return new JwtAuthenticationFiliter();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -33,8 +47,9 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests()
-                .antMatchers("/api/v1/auth").permitAll()
+                .antMatchers("/api/v1/auth/**").permitAll()
                 .and()
+                .addFilterBefore(jwtAuthenticationFiliter(), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
