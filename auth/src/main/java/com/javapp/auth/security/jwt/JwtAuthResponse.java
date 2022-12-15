f0720b472b9128b653fd2680dc9c6325d31e9ebb
llsrrll96 <llsrrll96@gmail.com>
2022-12-15 16:19:08 +0900

    chore: Add refreshtoken attr

--- auth/src/main/java/com/javapp/auth/security/jwt/JwtAuthResponse.java
+++ auth/src/main/java/com/javapp/auth/security/jwt/JwtAuthResponse.java
@@ -1,11 +1,28 @@
 package com.javapp.auth.security.jwt;
 
+import lombok.Builder;
+
 public class JwtAuthResponse {
     private String accessToken;
+    private String refreshToken;
     private String tokenType = "Bearer";
 
     public JwtAuthResponse(String accessToken){
         this.accessToken = accessToken;
+    }
+
+    @Builder
+    public JwtAuthResponse(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
     }
 
     public String getAccessToken(){
