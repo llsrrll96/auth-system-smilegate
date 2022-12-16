d02e8e9c4378402a406069c4cb85d66c39c516cf
llsrrll96 <llsrrll96@gmail.com>
2022-12-16 11:03:31 +0900

    chore: Move AuthorizationHeaderFilter.java

--- gateway/src/main/java/com/javapp/gateway/auth/AuthorizationHeaderFilter.java
+++ gateway/src/main/java/com/javapp/gateway/auth/AuthorizationHeaderFilter.java
@@ -1,17 +1,9 @@
-package com.javapp.gateway;
+package com.javapp.gateway.auth;
 
-import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.nio.charset.StandardCharsets;
 
 @Component
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
