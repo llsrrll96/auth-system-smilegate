20256cd2e9ffcd92ed987ff887edd234c0e84d57
llsrrll96 <llsrrll96@gmail.com>
2022-12-17 18:27:09 +0900

    feat: Add generateATandRT()

--- auth/src/main/java/com/javapp/auth/service/AuthServiceImpl.java
+++ auth/src/main/java/com/javapp/auth/service/AuthServiceImpl.java
@@ -1,8 +1,29 @@
 package com.javapp.auth.service;
 
+import com.javapp.auth.domain.User;
+import com.javapp.auth.security.jwt.JwtTokenProvider;
+import com.javapp.auth.security.jwt.models.Token;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+
+@RequiredArgsConstructor
 @Service
 public class AuthServiceImpl implements AuthService{
 
+    private final JwtTokenProvider tokenProvider;
+
+    @Override
+    public Token generateATandRT(User user) {
+        String accessToken = tokenProvider.generateAccessToken(user);
+        String refreshToken = tokenProvider.generateRefreshToken(user);
+
+        return Token.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .generateAtDateTime(LocalDateTime.now())
+                .build();
+    }
 }
