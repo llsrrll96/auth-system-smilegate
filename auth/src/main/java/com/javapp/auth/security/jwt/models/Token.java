b49e1402009de944bcf49cc0ad2c5a13cf534b6f
llsrrll96 <llsrrll96@gmail.com>
2022-12-17 18:27:31 +0900

    feat: Create Token

---
+++ auth/src/main/java/com/javapp/auth/security/jwt/models/Token.java
@@ -0,0 +1,38 @@
+package com.javapp.auth.security.jwt.models;
+
+import com.javapp.auth.domain.User;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Entity(name = "token")
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tokenId;
+
+    @OneToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String accessToken;
+
+    @Column(nullable = false, unique = true)
+    private String refreshToken;
+
+    @Column(nullable = false)
+    private LocalDateTime generateAtDateTime;
+
+    @Builder
+    public Token(User user, String accessToken, String refreshToken, LocalDateTime generateAtDateTime) {
+        this.user = user;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.generateAtDateTime = generateAtDateTime;
+    }
+}
