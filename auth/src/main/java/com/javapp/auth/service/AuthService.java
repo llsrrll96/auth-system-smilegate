011562e3220849ded5304dabe14330c14d6a2cd7
llsrrll96 <llsrrll96@gmail.com>
2022-12-18 12:25:26 +0900

    feat: Add refreshToken()

--- auth/src/main/java/com/javapp/auth/service/AuthService.java
+++ auth/src/main/java/com/javapp/auth/service/AuthService.java
@@ -1,5 +1,17 @@
 package com.javapp.auth.service;
 
+import com.javapp.auth.domain.User;
+import com.javapp.auth.dto.RefreshJwtDto;
+import com.javapp.auth.security.jwt.JwtAuthResponse;
+import com.javapp.auth.security.jwt.models.Token;
+
 public interface AuthService {
 
+    Token generateATandRT(User user);
+
+    JwtAuthResponse refreshToken(RefreshJwtDto refreshJwtDto);
+
+    // RT 확인후 AT 만 재발급
+
+    // RT 만료시키기
 }
--- auth/src/main/java/com/javapp/auth/service/AuthServiceImpl.java
+++ auth/src/main/java/com/javapp/auth/service/AuthServiceImpl.java
@@ -1,8 +1,12 @@
 package com.javapp.auth.service;
 
 import com.javapp.auth.domain.User;
+import com.javapp.auth.domain.repository.UserJpaRepository;
+import com.javapp.auth.dto.RefreshJwtDto;
+import com.javapp.auth.security.jwt.JwtAuthResponse;
 import com.javapp.auth.security.jwt.JwtTokenProvider;
 import com.javapp.auth.security.jwt.models.Token;
+import com.javapp.auth.security.jwt.repository.TokenJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +16,8 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 @Service
 public class AuthServiceImpl implements AuthService{
-
+    private final UserJpaRepository userJpaRepository;
+    private final TokenJpaRepository tokenJpaRepository;
     private final JwtTokenProvider tokenProvider;
 
     @Override
@@ -20,10 +25,29 @@ public class AuthServiceImpl implements AuthService{
         String accessToken = tokenProvider.generateAccessToken(user);
         String refreshToken = tokenProvider.generateRefreshToken(user);
 
+        // DB 작업도 여기해야 될듯
+
         return Token.builder()
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
                 .generateAtDateTime(LocalDateTime.now())
+                .build();
+    }
+
+    @Override
+    public JwtAuthResponse refreshToken(RefreshJwtDto refreshJwtDto) {
+        // RT 만료 확인 후 AT 재발급 else AT RT 재발급
+        Token token = tokenJpaRepository.findByAccessToken(refreshJwtDto.getAccessToken());
+        boolean isValidToken = tokenProvider.validateRefreshToken(token.getRefreshToken());
+        if(isValidToken){
+            //AT
+        }
+
+        Token newToken = generateATandRT(token.getUser());
+
+        return JwtAuthResponse.builder()
+                .accessToken(newToken.getAccessToken())
+                .refreshToken(newToken.getRefreshToken())
                 .build();
     }
 }
