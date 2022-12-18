b5c4cd3762c482998273675767f50bb4a2d92eca
llsrrll96 <llsrrll96@gmail.com>
2022-12-18 19:56:30 +0900

    feat: Add findByAccessToken , deleteByAccessToken

--- auth/src/main/java/com/javapp/auth/security/jwt/repository/TokenJpaRepository.java
+++ auth/src/main/java/com/javapp/auth/security/jwt/repository/TokenJpaRepository.java
@@ -3,12 +3,14 @@ package com.javapp.auth.security.jwt.repository;
 import com.javapp.auth.domain.User;
 import com.javapp.auth.security.jwt.models.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
+
+import java.util.Optional;
 
 public interface TokenJpaRepository extends JpaRepository<Token, Long> {
 
-    @Modifying
     int deleteByUser(User user);
 
-    Token findByAccessToken(String accessToken);
+    Optional<Token> findByAccessToken(String accessToken);
+
+    int deleteByAccessToken(String accessToken);
 }
