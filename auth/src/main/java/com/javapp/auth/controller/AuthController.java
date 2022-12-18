0e4ecf69a72ab57171bd2d87ba1b135ec0225f18
llsrrll96 <llsrrll96@gmail.com>
2022-12-18 19:52:43 +0900

    feat: Add refresh, logout

--- auth/src/main/java/com/javapp/auth/controller/AuthController.java
+++ auth/src/main/java/com/javapp/auth/controller/AuthController.java
@@ -1,12 +1,36 @@
 package com.javapp.auth.controller;
 
+import com.javapp.auth.dto.RefreshJwtDto;
+import com.javapp.auth.security.jwt.JwtAuthResponse;
+import com.javapp.auth.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.ws.rs.Path;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthService authService;
+
+    // 로그인 유지
+    // RT 만료 확인 후 AT 재발급 else AT RT 재발급
+    @PostMapping("/refresh")
+    public ResponseEntity<JwtAuthResponse> refresh(@RequestBody RefreshJwtDto refreshJwtDto){
+        JwtAuthResponse jwtAuthResponse = authService.refreshToken(refreshJwtDto);
+        return new ResponseEntity<>(jwtAuthResponse, HttpStatus.OK);
+    }
+
+
+    // 로그인 유지 X
+    // RT 만료 시키기(로그아웃)
+    @DeleteMapping("/logout")
+    public ResponseEntity<Boolean> logout(@RequestBody RefreshJwtDto refreshJwtDto){
+        boolean isSuccess = authService.deleteToken(refreshJwtDto);
+        return new ResponseEntity<>(isSuccess,HttpStatus.OK);
+    }
 
 }
