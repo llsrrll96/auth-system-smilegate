466e0613c6888d8259cf79ab00520bd7538ce519
llsrrll96 <llsrrll96@gmail.com>
2022-12-18 19:54:13 +0900

    feat: Implement GlobalResponseEntityExceptionHandler

--- auth/src/main/java/com/javapp/auth/exception/GlobalResponseEntityExceptionHandler.java
+++ auth/src/main/java/com/javapp/auth/exception/GlobalResponseEntityExceptionHandler.java
@@ -1,7 +1,25 @@
 package com.javapp.auth.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-public class GlobalResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+import java.util.Date;
 
+@ControllerAdvice
+public class GlobalResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(TokenNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleTokenNotFoundException(TokenNotFoundException exception, WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(),exception.getStatus(),exception.getCode(),exception.getMessage());
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND );
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleTokenNotFoundException(UserNotFoundException exception, WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(),exception.getStatus(),exception.getCode(),exception.getMessage());
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND );
+    }
 }
