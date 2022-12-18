ce44b70557e99124329d8fe22f98d087a3657779
llsrrll96 <llsrrll96@gmail.com>
2022-12-18 19:57:39 +0900

    feat: Add Attr
    String code, int status
    remove HttpStatus

--- auth/src/main/java/com/javapp/auth/exception/UserNotFoundException.java
+++ auth/src/main/java/com/javapp/auth/exception/UserNotFoundException.java
@@ -1,25 +1,19 @@
 package com.javapp.auth.exception;
 
-import org.springframework.http.HttpStatus;
+import lombok.Getter;
 
+@Getter
 public class UserNotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
     private final String message;
-    private final HttpStatus httpStatus;
+    private final String code;
+    private final int status;
 
-    public UserNotFoundException(String message, HttpStatus httpStatus) {
-        this.message = message;
-        this.httpStatus = httpStatus;
-    }
-
-    public HttpStatus getStatus() {
-        return httpStatus;
-    }
-
-    @Override
-    public String getMessage() {
-        return message;
+    public UserNotFoundException(ErrorCode errorCode){
+        this.message = errorCode.getMessage();
+        this.status = errorCode.getStatus();
+        this.code = errorCode.getCode();
     }
 }
