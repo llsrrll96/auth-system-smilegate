9a19a32ec1b4d26409f18414cc2e4971d3168c88
llsrrll96 <llsrrll96@gmail.com>
2022-12-18 12:28:17 +0900

    feat: modify to TokenJpaRepository

--- auth/src/main/java/com/javapp/auth/security/jwt/repository/TokenJpaRepository.java
+++ auth/src/main/java/com/javapp/auth/security/jwt/repository/TokenJpaRepository.java
@@ -1,15 +1,14 @@
 package com.javapp.auth.security.jwt.repository;
 
 import com.javapp.auth.domain.User;
-import com.javapp.auth.security.jwt.models.RefreshToken;
+import com.javapp.auth.security.jwt.models.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 
-import java.util.Optional;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+public interface TokenJpaRepository extends JpaRepository<Token, Long> {
 
     @Modifying
     int deleteByUser(User user);
+
+    Token findByAccessToken(String accessToken);
 }
